4d4a4d675333f81959100b21dca128ec674c8c80
Niladri Das <125604915+bniladridas@users.noreply.github.com>
2025-10-08 22:42:41 +0530

    chore: clean duplicate files and rebuild dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./utils/pathUtils.js";
-export { processFileWithImports } from "./examples/pathHandling.js";
-export type { ProcessedFile } from "./types.js";
-export type { ProcessedFile as ProcessedFileType } from "./types.js";
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./utils/pathUtils.js";
-export { processFileWithImports } from "./examples/pathHandling.js";
-//# sourceMappingURL=index.js.map
