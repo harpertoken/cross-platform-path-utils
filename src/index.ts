121ef1f74d2bc157e1125d9e3ddd4faeb8eb38de
Niladri Das <125604915+bniladridas@users.noreply.github.com>
2025-10-08 22:52:44 +0530

    refactor: remove examples export from main index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./utils/pathUtils.js";
 
-export { processFileWithImports } from "./examples/pathHandling.js";
 export type { ProcessedFile } from "./types.js";
 
 // Re-export types for backward compatibility
