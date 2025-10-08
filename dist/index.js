46907048a372ef8ed2f8816db15d154347ca804f
Niladri Das <125604915+bniladridas@users.noreply.github.com>
2025-10-08 22:38:08 +0530

    chore: remove broken lint script and update dist

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
-export * from "./utils/pathUtils";
-export { processFileWithImports } from "./examples/pathHandling";
+export * from "./utils/pathUtils.js";
+export { processFileWithImports } from "./examples/pathHandling.js";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from "./utils/pathUtils";
+export * from "./utils/pathUtils.js";
 
-export { processFileWithImports } from "./examples/pathHandling";
-export type { ProcessedFile } from "./types";
+export { processFileWithImports } from "./examples/pathHandling.js";
+export type { ProcessedFile } from "./types.js";
 
 // Re-export types for backward compatibility
-export type { ProcessedFile as ProcessedFileType } from "./types";
+export type { ProcessedFile as ProcessedFileType } from "./types.js";
